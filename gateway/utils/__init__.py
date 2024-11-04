9f83ce6b077319d6d5b88e1f19cad6889649655d
Данила Чуприн <danilacuprin@MacBook-Pro-Danila.local>
2024-11-04 19:15:23 +0300

    Added retry decorator

--- gateway/utils/__init__.py
+++ gateway/utils/__init__.py
@@ -2,10 +2,12 @@ from .converter import Converter
 from .amqp_client import AMQPClient
 from .singleton import SingletonMeta
 from .redis_ import RedisCache
+from .retry import retry
 
 __all__: list[str] = [
     "Converter",
     "AMQPClient",
     "SingletonMeta",
     "RedisCache",
+    "retry",
 ]
