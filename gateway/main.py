7bf3cf27113cb6ad89614712b5b8ec279cdfdb52
Данила Чуприн <danilacuprin@MacBook-Pro-Danila.local>
2024-11-05 10:57:56 +0300

    Moved gateway host, port to .env

--- gateway/main.py
+++ gateway/main.py
@@ -2,6 +2,7 @@ import fastapi
 from api import ImageProcessorRouter, AuthRouter
 from exceptions import ExceptionHandlers
 import uvicorn
+import settings
 
 
 app = fastapi.FastAPI()
@@ -22,4 +23,6 @@ for router in routers:
     app.include_router(router.create_router())
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app", host=settings.GATEWAY_HOST, port=settings.GATEWAY_PORT, reload=True
+    )
